5d35b8b9d8d4e7673ab0189fb4feb6062f919bc3
LSH <dlrmld7@gmail.com>
2018-11-04 15:45:47 +0900

    jslint 제거 및 login 페이지 예시 작성

---
+++ src/page/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="login_workspace">
+        <span>아이디</span><input type="text">
+        <span>비밀번호</span><input type="password">
+        <button>로그인</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
+
+<style lang="scss">
+    #login_workspace {
+        background-color: red;
+        width: 100px;
+        height: 100px;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/page/Login'
+import Error from '@/page/Error'
 
 Vue.use(Router)
 
@@ -8,9 +9,14 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '*',
+      name: 'Error',
+      component: Error
     }
   ]
 })
